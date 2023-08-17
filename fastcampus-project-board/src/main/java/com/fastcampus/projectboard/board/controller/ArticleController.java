d8dcabcd866fd207df3ffb11ca600483effc4205
Park Jongchul (DIKI) <maxjc@naver.com>
2023-08-17 13:58:30 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    명세에 정의된 방식대로 테스트

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/board/controller/ArticleController.java
@@ -0,0 +1,20 @@
+package com.fastcampus.projectboard.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * packageName   : com.fastcampus.projectboard.board.controller
+ * fileName     : ArticleController
+ * author       : danny
+ * date         : 2023-08-17
+ * description  :
+ * ===========================================================
+ * DATE             AUTHOR          NOTE
+ * -----------------------------------------------------------
+ * 2023-08-17          danny         최초 생성
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
